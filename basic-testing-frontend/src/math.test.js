2a40cdcd7597196c40888fb2cebd79f88775934b
Rafael Albano <albano_rafael15@hotmail.com>
2026-05-23 23:50:59 -0300

    feat: more tests scenarios

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -12,3 +12,22 @@ it('should summarize all numbers values in an array', () => {
   // Assert
   expect(result).toBe(expectedResults);
 })
+
+it('should yield NaN if a least one invalid number is provided', () => {
+  const inputs = ['invalid', 1];
+  const result = add(inputs);
+
+  expect(result).toBe(NaN);
+})
+
+it('should yield Na correct sum if an array of numeric string values is provided', () => {
+  // Arrange
+  const numbers = ['1', '2'];
+  const expectedResults = numbers.reduce((prevValue, curValue) => 
+    +prevValue + +curValue , 0)
+  // Act
+  const result = add(numbers);
+  
+  // Assert
+  expect(result).toBe(expectedResults);
+})
