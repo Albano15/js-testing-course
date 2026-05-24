ecdb93aa65910c5a5b070c7917146f01497eeecc
Rafael Albano <albano_rafael15@hotmail.com>
2026-05-24 11:54:18 -0300

    test: testing for errors

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,33 +1,64 @@
 import { expect, it } from "vitest";
-import { add } from "./math"
+import { add } from "./math";
 
-it('should summarize all numbers values in an array', () => {
+it("should summarize all numbers values in an array", () => {
   // Arrange
   const numbers = [1, 2];
-  const expectedResults = numbers.reduce((prevValue, curValue) => 
-    prevValue + curValue , 0)
+  const expectedResults = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0,
+  );
   // Act
   const result = add(numbers);
-  
+
   // Assert
   expect(result).toBe(expectedResults);
-})
+});
 
-it('should yield NaN if a least one invalid number is provided', () => {
-  const inputs = ['invalid', 1];
+it("should yield NaN if a least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
   const result = add(inputs);
 
   expect(result).toBe(NaN);
-})
+});
 
-it('should yield Na correct sum if an array of numeric string values is provided', () => {
+it("should yield correct sum if an array of numeric string values is provided", () => {
   // Arrange
-  const numbers = ['1', '2'];
-  const expectedResults = numbers.reduce((prevValue, curValue) => 
-    +prevValue + +curValue , 0)
+  const numbers = ["1", "2"];
+  const expectedResults = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0,
+  );
   // Act
   const result = add(numbers);
-  
+
   // Assert
   expect(result).toBe(expectedResults);
+});
+
+it("should yield 0 if an empty array is provided", () => {
+  let emptyArray = [];
+
+  const result = add(emptyArray);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if parameter isnt passed into the function", () => {
+  const result = () => {
+    add();
+  };
+
+  expect(result).toThrow();
+});
+
+it("should throw an error if provided with multiple arguments insted of array", () => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const result = () => {
+    add(num1, num2);
+  }
+
+  expect(result).toThrow();
 })
